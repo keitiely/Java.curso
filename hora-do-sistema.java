2eba6d2f7aa6094d6baa0e274ee7aae23d435bbf
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-08-29 15:06:00 -0300

    Create Hora-do-sistema.java

---
+++ hora-do-sistema.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.primeiroprograma;
+
+import java.util.Date;
+
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        
+       Date relogio = new Date();
+       System.out.println("A Hora do sistema e");
+       System.out.println(relogio.toString());
+    }
+}
