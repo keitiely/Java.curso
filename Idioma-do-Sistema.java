7c172ec93fe0a712f44f90732cca495a12b967c3
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-08-29 15:38:22 -0300

    Create Idioma-do-Sistema.java

---
+++ Idioma-do-Sistema.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.idiomadosistem;
+
+import java.util.Locale;
+
+
+public class IdiomaDoSistem {
+
+    public static void main(String[] args) {
+    Locale Loc = Locale.getDefault();
+     System.out.print("O idioma do Sistema e ");
+     System.out.println(Loc.getDisplayLanguage());//imprime Português
+ 
+    }
+}
